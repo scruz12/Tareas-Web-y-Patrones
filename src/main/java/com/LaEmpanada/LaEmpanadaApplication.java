be82485b1159bd81faf59b6eb083b458bd86942c
STEVEN <STEVEN@DESKTOP-EMQA0KG>
2022-06-08 20:59:06 -0600

    Tarea Semana 3 "La Empanada"

---
+++ src/main/java/com/LaEmpanada/LaEmpanadaApplication.java
@@ -0,0 +1,13 @@
+package com.LaEmpanada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaEmpanadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaEmpanadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LaEmpanada/LaEmpanadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LaEmpanada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaEmpanadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
